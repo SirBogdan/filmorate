faa15051a08f9e4472dd734819471a276a73a486
Богдан <bogdan-sirotyuk@yandex.ru>
2022-08-15 14:54:24 +0300

    Sprint 11 done 4-th commit

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -33,112 +33,161 @@ class FilmorateApplicationTests {
     private final GenreDbStorage genreDbStorage;
     private final MPADbStorage mpaDbStorage;
 
-    @Test
-    @Order(1)
-    public void userDbStorageTest() {
-        User user1 = new User(1L, "user1@mail.ru", "user1Login", "user1Name",
-                LocalDate.of(2001, 1, 1));
-        User user2 = new User(2L, "user2@mail.ru", "user2Login", "user2Name",
-                LocalDate.of(2002, 1, 1));
-        User user1Changed = new User(1L, "user1Changed@mail.ru", "user1ChangedLogin",
-                "user3ChangedName", LocalDate.of(2001, 2, 1));
-        User user3 = new User(3L, "user3@mail.ru", "user3Login", "user3Name",
-                LocalDate.of(2003, 1, 1));
+    private final User user1 = new User(1L, "user1@mail.ru", "user1Login", "user1Name",
+            LocalDate.of(2001, 1, 1));
+    private final User user2 = new User(2L, "user2@mail.ru", "user2Login", "user2Name",
+            LocalDate.of(2002, 1, 1));
+    private final User user1Changed = new User(1L, "user1Changed@mail.ru", "user1ChangedLogin",
+            "user3ChangedName", LocalDate.of(2001, 2, 1));
+    private final User user3 = new User(3L, "user3@mail.ru", "user3Login", "user3Name",
+            LocalDate.of(2003, 1, 1));
+    private final Film film1 = new Film(1L, "film1Name", "film1Description",
+            LocalDate.of(2001, 1, 1), 100, 1, new MPA(1, "Комедия"));
+    private final Film film2 = new Film(2L, "film2Name", "film2Description",
+            LocalDate.of(2002, 1, 1), 200, 2, new MPA(2, "Драма"));
+    private final Film film1Changed = new Film(1L, "film1film1ChangedName", "film1film1ChangedDescription",
+            LocalDate.of(2001, 2, 1), 101, 3, new MPA(3, "Мультфильм"));
+    private final Collection<Genre> genreCollection = List.of(
+            new Genre(1L, "Комедия"),
+            new Genre(2L, "Драма"),
+            new Genre(3L, "Мультфильм"),
+            new Genre(4L, "Триллер"),
+            new Genre(5L, "Документальный"),
+            new Genre(6L, "Боевик"));
+    private final Collection<MPA> mpaCollection = List.of(
+            new MPA(1L, "G"),
+            new MPA(2L, "PG"),
+            new MPA(3L, "PG-13"),
+            new MPA(4L, "R"),
+            new MPA(5L, "NC-17"));
 
+    @Test
+    @Order(11)
+    public void createUserTest() {
         userDbStorage.createUser(user1);
 
-        assertEquals(user1, userDbStorage.getUserById(1)); //тест методов createUser и getUserById
+        assertEquals(user1, userDbStorage.getUserById(1));
+    }
 
+    @Test
+    @Order(12)
+    public void getUserByIdTest() {
+        assertEquals(user1, userDbStorage.getUserById(1));
+    }
+
+    @Test
+    @Order(13)
+    public void findAllUsersTest() {
         userDbStorage.createUser(user2);
         Collection<User> userCollection = List.of(user1, user2);
 
-        assertEquals(userCollection, userDbStorage.findAll()); // тест метода findAll
+        assertEquals(userCollection, userDbStorage.findAll());
+    }
 
+    @Test
+    @Order(14)
+    public void putUserTest() {
         userDbStorage.putUser(user1Changed);
 
-        assertEquals(user1Changed, userDbStorage.getUserById(1)); // тест метода putUser
+        assertEquals(user1Changed, userDbStorage.getUserById(1));
+    }
 
+    @Test
+    @Order(21)
+    public void addFriendTest() {
         friendsDbStorage.addFriend(1L, 2L);
 
-        assertEquals(List.of(user2), friendsDbStorage.getUserFriends(1L)); //тест методов addFriend и getUserFriends
+        assertEquals(List.of(user2), friendsDbStorage.getUserFriends(1L));
+    }
 
+
+    @Test
+    @Order(22)
+    public void getUserFriendsTest() {
         friendsDbStorage.deleteFriend(1L, 2L);
 
-        assertEquals(List.of(), friendsDbStorage.getUserFriends(1L)); // тест метода getUserFriends
+        assertEquals(List.of(), friendsDbStorage.getUserFriends(1L));
+    }
 
+    @Test
+    @Order(23)
+    public void findFriendsIntersectionTest() {
         userDbStorage.createUser(user3);
         friendsDbStorage.addFriend(1L, 3L);
         friendsDbStorage.addFriend(2L, 3L);
 
-        // тест метода findFriendsIntersection
         assertEquals(List.of(user3), friendsDbStorage.findFriendsIntersection(1L, 2L));
     }
 
     @Test
-    @Order(2)
-    public void filmDbStorageTest() {
-        Film film1 = new Film(1L, "film1Name", "film1Description",
-                LocalDate.of(2001, 1, 1), 100, 1, new MPA(1, "Комедия"));
-        Film film2 = new Film(2L, "film2Name", "film2Description",
-                LocalDate.of(2002, 1, 1), 200, 2, new MPA(2, "Драма"));
-        Film film1Changed = new Film(1L, "film1film1ChangedName", "film1film1ChangedDescription",
-                LocalDate.of(2001, 2, 1), 101, 3, new MPA(3, "Мультфильм"));
-
+    @Order(31)
+    public void createFilmTest() {
         filmDbStorage.createFilm(film1);
 
-        assertEquals(film1, filmDbStorage.getFilmById(1)); // тест методов createFilm и getFilmById
+        assertEquals(film1, filmDbStorage.getFilmById(1));
+    }
 
+    @Test
+    @Order(32)
+    public void getFilmByIdTest() {
+        assertEquals(film1, filmDbStorage.getFilmById(1));
+    }
+
+    @Test
+    @Order(33)
+    public void findAllFilmsTest() {
         filmDbStorage.createFilm(film2);
         Collection<Film> filmCollection = List.of(film1, film2);
 
-        assertEquals(filmCollection, filmDbStorage.findAll()); // тест метода findAll
+        assertEquals(filmCollection, filmDbStorage.findAll());
+    }
 
+    @Test
+    @Order(34)
+    public void putFilmTest() {
         filmDbStorage.putFilm(film1Changed);
 
-        assertEquals(film1Changed, filmDbStorage.getFilmById(1)); // тест метода putFilm
+        assertEquals(film1Changed, filmDbStorage.getFilmById(1));
     }
 
     @Test
-    @Order(3)
-    public void genreDbStorageTest() {
-        Collection<Genre> genreCollection = List.of(
-                new Genre(1L, "Комедия"),
-                new Genre(2L, "Драма"),
-                new Genre(3L, "Мультфильм"),
-                new Genre(4L, "Триллер"),
-                new Genre(5L, "Документальный"),
-                new Genre(6L, "Боевик"));
-
-        assertEquals(genreCollection, genreDbStorage.getAllGenres()); //тест метода getAllGenres
-        assertEquals(new Genre(2L, "Драма"), genreDbStorage.getGenreById(2L)); //тест метода getGenreById
+    @Order(41)
+    public void findMostLikedFilmsTest() {
+        Film filmActual = likeDbStorage.findMostLikedFilms(1).stream().findAny().get();
+        assertEquals(1L, filmActual.getId());
     }
 
     @Test
-    @Order(4)
-    public void mpaDbStorageTest() {
-        Collection<MPA> mpaCollection = List.of(
-                new MPA(1L, "G"),
-                new MPA(2L, "PG"),
-                new MPA(3L, "PG-13"),
-                new MPA(4L, "R"),
-                new MPA(5L, "NC-17"));
-
-        assertEquals(mpaCollection, mpaDbStorage.getAllMPA()); // тест метода getAllMPA
-        assertEquals(new MPA(3L, "PG-13"), mpaDbStorage.getMPAById(3L));  // тест метода getMPAById
-    }
-
-    @Test
-    @Order(10)
-    public void likeDbStorageTest() {
-        Film film1Changed = likeDbStorage.findMostLikedFilms(1).stream().findAny().get();
-        assertEquals(1L, film1Changed.getId()); // тест метода findMostLikedFilms
-
-
+    @Order(42)
+    public void addLikeTest() {
         likeDbStorage.addLike(1L, 1L);
-        assertEquals(4, filmDbStorage.getFilmById(1).getRate()); // тест метода addLike
-
-        likeDbStorage.deleteLike(1L, 1L);
-        assertEquals(3, filmDbStorage.getFilmById(1).getRate()); // тест метода deleteLike
+        assertEquals(4, filmDbStorage.getFilmById(1).getRate());
     }
 
+    @Test
+    @Order(43)
+    public void deleteLikeTest() {
+        likeDbStorage.deleteLike(1L, 1L);
+        assertEquals(3, filmDbStorage.getFilmById(1).getRate());
+    }
+
+    @Test
+    public void getAllGenresTest() {
+        assertEquals(genreCollection, genreDbStorage.getAllGenres());
+    }
+
+    @Test
+    public void getGenreByIdTest() {
+        assertEquals(new Genre(2L, "Драма"), genreDbStorage.getGenreById(2L));
+    }
+
+    @Test
+    public void getAllMPATest() {
+        assertEquals(mpaCollection, mpaDbStorage.getAllMPA());
+    }
+
+    @Test
+    public void getMPAByIdTest() {
+        assertEquals(new MPA(3L, "PG-13"), mpaDbStorage.getMPAById(3L));
+    }
 }
